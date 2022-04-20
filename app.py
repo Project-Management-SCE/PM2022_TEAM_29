f8c9a9b37be84f6f1275c16b2b1df1248f7ea5c1
sujood eldda <73096327+SujoodEldda@users.noreply.github.com>
2022-04-20 11:20:06 +0300

    home

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template,request,flash,session,redirect,url_for,abort
+import sqlite3
+app = Flask(__name__)
+
+@app.route('/home')
+def hello():
+    return "<h1>Hello World!  a<h1>"
+if __name__ == "__main__":
+    app.run()

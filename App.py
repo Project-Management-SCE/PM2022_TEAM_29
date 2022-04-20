70f47b4d9c4b1e54f7b5d6cd7a567801d2d755fd
sujood eldda <73096327+SujoodEldda@users.noreply.github.com>
2022-04-20 11:09:43 +0300

    Revert "add the home page"
    This reverts commit 9fd5d11edeb82310043a43d0d5c607da63814019.

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
 import sqlite3
-
+from forms import signupForm
 
 app = Flask(__name__)
 
@@ -32,6 +32,39 @@ app.config['SECRET_KEY'] = 'Sujood'
 def home():
     return render_template('home.html')
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    Database()
+    form = signupForm()
+    if request.method == 'POST':
+        username = form.username.data
+        password = form.password.data
+        name = form.name.data
+        age = form.age.data
+        location = form.location.data
+        phone = form.phone.data
+        hour = form.hour.data
+        hobby = form.hobby.data
+        cursor.execute("SELECT * FROM `volunteer` WHERE `username` = ?", (username,))
+        if cursor.fetchone() is not None:
+            flash("username already exist")
+            return render_template('register.html', form=form)
+        else:
+            insert_volunteer(str(username), str(password), str(name), str(age), str(location), str(phone), str(hobby))
+            cursor.close()
+            conn.close()
+    flash("successfully created!")
+    return render_template('register.html', form=form)
+
+
+def insert_volunteer(username, password, age, location, phone, name, hobby):
+    try:
+        cursor.execute("INSERT INTO `volunteer` (username, password, age, location, phone, name, hobby) VALUES(?, ?, ?,?,?,?,?)",
+                       (username, password, age, location, phone, name, hobby))
+        conn.commit()
+    except ValueError:
+        print(ValueError)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -8,3 +8,13 @@ from wtforms.validators import DataRequired,Length,EqualTo, Email
 from flask_wtf import FlaskForm
 
 
+class signupForm(FlaskForm):
+    username = StringField(label='username', validators=[DataRequired(), Length(min=4, max=50)])
+    password = PasswordField(label='password', validators=[DataRequired(), Length(min=5, max=8)])
+    name = StringField(label='name', validators=[DataRequired(), Length(min=3, max=50)])
+    age = StringField(label='age', validators=[DataRequired(), Length(min=1, max=20)])
+    location = StringField(label='location', validators=[DataRequired(), Length(min=3, max=50)])
+    phone = StringField(label='phone', validators=[DataRequired(), Length(min=3, max=20)])
+    hour = StringField(label='hour', validators=[DataRequired(), Length(min=3, max=20)])
+    hobby = StringField(label='hobby', validators=[DataRequired(), Length(min=3, max=50)])
+    submit = SubmitField(label='Sign up')

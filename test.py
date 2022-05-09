b77ee75cac4a119313af160738320924d501c870
sujood eldda <73096327+SujoodEldda@users.noreply.github.com>
2022-05-09 13:32:42 +0300

    test_registerFall

--- test.py
+++ test.py
@@ -21,7 +21,21 @@ class MyTestCase(unittest.TestCase):
         except:
             self.assertTrue(False)
     
-
+    def test_fall_register(self):
+        App.Database()
+        try:
+            username = "sujood"
+            password = "123445"
+            name = "sama"
+            age = 23
+            location = "arad"
+            phone = "0433433443"
+            hour = 15
+            hobby = "reading and helping people"
+            App.insert_volunteer(str(username), str(password), str(name), str(age), str(location), str(phone), str(hobby))
+            self.assertFalse(True)
+        except:
+            self.assertFalse(False)
 
 
 if __name__ == '__main__':

c11b1eaf65f8858b6a040232cb18ad52a3a1fc17
sujood eldda <73096327+SujoodEldda@users.noreply.github.com>
2022-05-09 13:31:47 +0300

    test_register1

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 import unittest
+from flask import app
+from flask_wtf import form
+import App
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_register(self):
+        App.Database()
+        try:
+            username = "sujood"
+            password = "123445"
+            name = "sama"
+            age = 23
+            location = "arad"
+            phone = "0433433443"
+            hour = 15
+            hobby = "reading and helping people"
+            App.insert_volunteer(str(username), str(password), str(name), str(age), str(location), str(phone), str(hobby))
+            self.assertTrue(True)
+        except:
+            self.assertTrue(False)
+    
+
 
 
 if __name__ == '__main__':

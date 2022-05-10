297baf569f890901515ea0f8109dc03b7fad6f52
NoorNaif <noorab3@ac.sce.ac.il>
2022-05-10 10:41:14 +0300

    BSPM2022t29-97

--- App.py
+++ App.py
@@ -362,5 +362,15 @@ def divide_donation(donation, username):
     cursor.execute("UPDATE 'organization' SET donation=? WHERE username=?", (donation, username,))
     conn.commit()
 
+def updateHours():
+    form = UpdateHours()
+
+    if request.method == 'POST':
+        donation = form.hours.data
+        cursor.execute("UPDATE 'Volunteer' SET donation=? WHERE username=?", (hour, session["useradm"],))
+        conn.commit()
+    flash("successfully updated!")
+    return render_template('updateHours.html', form=form, )
+
 if __name__ == '__main__':
     app.run(debug=True)
